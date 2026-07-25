f66d22070d0b87066607e96a97ad098313762027
Nangaaoay246 <jan.michael.aoay246@adamson.edu.ph>
2026-07-25 23:08:54 +0800

    added constants.py

---
+++ constants.py
@@ -0,0 +1,29 @@
+# Profile -------------------------------------------------------------------------------------------------------------------------------------
+profile = {
+            'name':'Jan Michael Aoay', 
+            'title': 'Python Developer & Machine Learning Engineer',
+            'image_path': 'https://github.com/Nangaaoay246/Portfolio/blob/main/assets/profile1.jpg?raw=true',
+            "location": '🌏 Cavite, Philippines',
+            "email": '✉️ aoay.janmichael@gmail.com',
+            "phone": '📞 0976 339 2122'
+        }
+
+# Social Media Links ------------------------------------------------------------------------------------------------------------------------
+links = [
+    {
+        "name": "LinkedIn",
+        "url": "https://www.linkedin.com/in/jan-michael-aoay/",
+        "icon": "fab fa-linkedin"
+    },
+    {
+        "name": "GitHub",
+        "url": "https://github.com/Nangaaoay246",
+        "icon": "fab fa-github"
+    },
+    {
+        "name": "Google Scholar",
+        "url": "https://scholar.google.com/citations?user=ttu2404AAAAJ&hl=en&authuser=2",
+        "icon": "fab fa-google"
+    },
+]
+
