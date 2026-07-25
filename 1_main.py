14998fb6571ddfc4683bf4274ee3ca525a9c3cd6
Nangaaoay246 <jan.michael.aoay246@adamson.edu.ph>
2026-07-25 23:11:16 +0800

    added main page

---
+++ 1_main.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import components.sidebar as sidebar
+
+def configure():
+    st.set_page_config(
+         page_title='Money Transfer Fraud Detection',
+            page_icon='🚨',
+            layout='wide',
+            initial_sidebar_state='expanded'
+    ) 
+
+def load_css():
+    st.markdown("""
+    <link rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    """, unsafe_allow_html=True)
+
+    with open("styles.css") as f:
+        st.markdown(
+            f"<style>{f.read()}</style>",
+            unsafe_allow_html=True
+    )
+    
+st.sidebar.success("Select a demo above.")
+
+
+if __name__ == '__main__':
+
+    configure()
+    load_css()
+    sidebar()
