8e291947181c17f56ac1704a5acc8f036c1db0cc
nanaaoay246 <aoay.janmichael@gmail.com>
2026-07-26 07:14:04 -0700

    added project overview

--- 1_main.py
+++ 1_main.py
@@ -1,11 +1,11 @@
 import streamlit as st
 from components.sidebar import sidebar
+from constants import page_content
 
 def configure():
     st.set_page_config(
          page_title='Money Transfer Fraud Detection',
             page_icon='🚨',
-            layout='wide',
             initial_sidebar_state='expanded'
     ) 
 
@@ -16,12 +16,35 @@ def pages():
     return st.navigation([main_page, model_page])
 
 def projectOverview():
-    st.title('Money Transfer Fraud Detection - Project Overview')
+    st.image('assets/scam.jpg', width='stretch')
+    st.title('Money Transfer Fraud Detection')
+
+    st.header('Project Overview', divider='green')
+    st.markdown(page_content['ProjectOverview_1'])
+    col_img1, col_text1 = st.columns([1, 2], vertical_alignment="center")
+    with col_img1:
+        st.image('assets/Smiski-wallet.jpg', width='stretch', caption='My everyday wallet, whose net worth is about ₱150')
+    with col_text1:
+        st.markdown(page_content['ProjectOverview_2'])
+    col_text2, col_img2 = st.columns([2, 1], vertical_alignment="center")
+    with col_img2:
+        st.image('assets/bank-transfer.jpg', width='stretch', caption='Ate QR nalang po plz')
+    with col_text2:
+        st.markdown(page_content['ProjectOverview_3'])
+    st.markdown(page_content['projectOverview_4'])
+
+def problemStatement():
+    st.header('Problem Statement',  divider='green')
+
+def content():
+    projectOverview()
+    problemStatement()
+    
 
 if __name__ == '__main__':
     configure()
     
-    main_page = st.Page(projectOverview, title="Project Overview", icon="📒", default=True)
+    main_page = st.Page(content, title="Project Overview", icon="📒", default=True)
     model_page = st.Page("2_model.py", title="Fraud Detection App", icon="🤖")
 
     pg = st.navigation([main_page, model_page])
--- constants.py
+++ constants.py
@@ -32,3 +32,24 @@ links = [
     }
 ]
 
+page_content = {
+    'ProjectOverview_1': """
+As a newly minted young adult (read: fresh graduate), I’ve been spending most of my time signing up for banks and e-wallets where I can store my future (read: currently non-existent) salary once I finally start working. Lately, :blue[**BDO**] and :orange[**MariBank**] have become my go-tos. Keeping most of my allowance in digital wallets really hammered home just how cashless the Philippines has become.
+    """,
+
+    'ProjectOverview_2': """ 
+These days, I rarely carry cash whenever I leave the house. Maybe I’ll bring a hundred and fifty pesos in my :green[**smiski**]-keychained wallet; just enough to cover my daily commute to and from Manila. Everything else? I have it on my phone. Whether I’m buying coffee from :red[**Kape Kuripot**], grabbing lunch in :yellow[**McDo**], or stopping by :blue[**Lawson**] to grab myself a bottle of Mogu-mogu, my default question is always, **"Ate, may QR po?"** A quick scan, a few taps, and I'm done. It’s so seamless that I stopped thinking about the mechanics behind it.
+    """,
+
+    'ProjectOverview_3': """ 
+    ...But, that convenience comes with a catch.
+
+Behind every :blue[instant] :red[transfer] lies a massive financial pipeline that processes millions upon millions of transactions daily. As more Filipinos go digital, the surface area for fraud grows right alongside us. The very tech that makes paying for coffee effortless also gives scammers a platform to exploit a lot of people.
+    """,
+    'projectOverview_4': """
+That realization was the spark for this project. While we only experience the slick, two-second front end of a digital payment, financial institutions are running a non-stop triage behind the scenes to separate legitimate transfers from scams.
+
+To explore how banks can tackle this, this project compares the effectiveness of `Logistic Regression`, `Decision Trees`, and `Random Forest` models in classifying fraudulent behavior using standard transactional data available to banks.
+    """ 
+}
+
